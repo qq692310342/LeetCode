ca86abdc91c944465c4b50688c00a21e6ae67d66
weihuangcheng <weihuangcheng@hikvision.com.cn>
2020-08-06 18:51:33 +0800

    add LeetCode算法题测试

---
+++ src/agu/Day04.java
@@ -0,0 +1,49 @@
+package cn.jeff.agu;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Author weihuangcheng
+ * @Date 2020/8/4 8:53
+ * @Version 1.0
+ * 题目207：
+ * 你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+ * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+ * 给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+ *
+ *
+ * 思路：拓扑排序
+ * 本题可约化为： 课程安排图是否是 有向无环图(DAG)。即课程间规定了前置条件，但不能构成任何环路，否则课程前置条件将不成立。
+ * 思路是通过 拓扑排序 判断此课程安排图是否是 有向无环图(DAG) 。 拓扑排序原理： 对 DAG 的顶点进行排序，
+ * 使得对每一条有向边 (u, v)(u,v)，均有 uu（在排序记录中）比 vv 先出现。亦可理解为对某点 vv 而言，只有当 vv 的所有源点均出现了，vv 才能出现。
+ * 通过课程前置条件列表 prerequisites 可以得到课程安排图的 邻接表 adjacency，以降低算法时间复杂度，以下两种方法都会用到邻接表。
+ */
+public class Day04 {
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegrees = new int[numCourses];
+        List<List<Integer>> adjacency = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++)
+            adjacency.add(new ArrayList<>());
+        // Get the indegree and adjacency of every course.
+        for(int[] cp : prerequisites) {
+            indegrees[cp[0]]++;
+            adjacency.get(cp[1]).add(cp[0]);
+        }
+        // Get all the courses with the indegree of 0.
+        for(int i = 0; i < numCourses; i++)
+            if(indegrees[i] == 0) queue.add(i);
+        // BFS TopSort.
+        while(!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            for(int cur : adjacency.get(pre))
+                if(--indegrees[cur] == 0) queue.add(cur);
+        }
+        return numCourses == 0;
+    }
+}
---
+++ src/agu/Day06.java
@@ -0,0 +1,22 @@
+package cn.jeff.agu;
+
+import java.util.List;
+
+/**
+ * @Author weihuangcheng
+ * @Date 2020/8/6 9:28
+ * @Version 1.0
+ * 336
+ * 给定一组唯一的单词， 找出所有不同 的索引对(i, j)，使得列表中的两个单词， words[i] + words[j] ，可拼接成回文串。
+ * 输入: ["abcd","dcba","lls","s","sssll"]
+ * 输出: [[0,1],[1,0],[3,2],[2,4]]
+ * 解释: 可拼接成的回文串为 ["dcbaabcd","abcddcba","slls","llssssll"]
+ *
+ */
+public class Day06 {
+
+    public List<List<Integer>> palindromePairs(String[] words) {
+        return null;
+
+    }
+}
